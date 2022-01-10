d03ceb35d8513506984feb5e5b6485d1f2355f2e
TheCrunchy <crunchycamsta@gmail.com>
2022-01-10 17:33:48 +0000

    .

--- Template/Properties/AssemblyInfo.cs
+++ Template/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GravityBlockDisable")]
+[assembly: AssemblyTitle("Template")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GravityBlockDisable")]
+[assembly: AssemblyProduct("Template")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
